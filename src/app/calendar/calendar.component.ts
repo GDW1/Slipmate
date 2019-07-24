500af6bcd74f01397dc28999b3d546f506007385
Guy <guywilks@guys-mbp-2.attlocal.net>
2019-07-24 10:01:33 -0700

    Celendar work

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ApiService} from '../api.service';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  private FirstFormGroup: FormGroup;
+  private month: any;
+  private day: any;
+  private response: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder,
+              private api: ApiService,
+              private ngZone: NgZone,
+              private loginService: LoginService) {
   }
 
+  ngOnInit() {
+    this.FirstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+  }
+
+  submitCalendarDate() {
+    let date = document.forms[0].elements['date'].value;
+    this.month = date.split('/')[0];
+    this.day = date.split('/')[1];
+    if (parseInt(this.month) < 10) {
+      this.month = '0' + this.month;
+    }
+    if (parseInt(this.day) < 10) {
+      this.day = '0' + this.day;
+    }
+    console.log(this.month + ":" + this.day);
+    if (this.day === '' || this.month === '') {
+      // tslint:disable-next-line:max-line-length
+      console.log(this.month, this.day);
+    } else {
+      this.response = this.api.createBlockedDay(this.loginService.user.email.split('@')[0], this.month, this.day);
+    }
+
+  }
 }
