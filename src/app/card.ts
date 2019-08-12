0aa0dbc3ebf116a8c155ef20328e44e5c4a01875
Guy <guywilks@Guys-MBP-2.attlocal.net>
2019-08-11 19:28:48 -0700

    Revert "Merge branch 'teacher-frontend' into guy-teacher-frontend"
    This reverts commit 1de18c58941b3e003b17c81421f670abd0697499, reversing
    changes made to 642ca60edc975964e30d2b12624e824948e99628.

--- src/app/card.ts
+++ src/app/card.ts
@@ -2,7 +2,6 @@ export default class Card {
     name: string;
     toTeach: string;
     fromTeach: string;
-    toTeachID: string;
     date: string;
     slipID: string;
     showButtons: boolean;
