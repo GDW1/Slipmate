6d077364955883ea6897166ea972cfda33db50be
asacco24 <saccoa54@gmail.com>
2019-08-14 09:16:50 -0700

    I have no idea what the heck happened I hope I didn't mess it up completely

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -27,35 +27,28 @@ export class CreateComponent implements OnInit {
     reason: string;
 
     constructor(private formBuilder: FormBuilder,
-                private api: ApiService,
-                private ngZone: NgZone) {
+                private api: ApiService) {
     }
 
-    loadSID() {
+    load() {
         this.studentID = document.forms[0].elements['studentID'].value;
-    }
 
-    loadDate() {
         let date = document.forms[1].elements['date'].value;
         this.month = date.split('/')[0];
         this.day = date.split('/')[1];
         if (parseInt(this.month) < 10) { this.month = '0' + this.month; }
         if (parseInt(this.day) < 10) this.day = '0' + this.day;
-    }
 
-    loadTeachers() {
         this.ttID = document.forms[2].elements['toTeachID'].value;
         this.ftID = document.forms[2].elements['fromTeachID'].value;
-    }
 
-    loadReason() {
         this.reason = document.forms[3].elements['reason'].value;
     }
 
     async submit() {
-        this.loadReason();
+        this.load();
+        this.gotError = false;
         this.loading = true;
-        this.ngZone.run(() => {});
 
         if (this.studentID.trim() === '' || this.ttID.trim() === '' || this.ftID.trim() === '' || this.day === '' || this.month === '') {
             // tslint:disable-next-line:max-line-length
@@ -64,13 +57,18 @@ export class CreateComponent implements OnInit {
             this.gotError = true;
             this.loading = false;
         } else {
-            this.api.createPass(true, this.ttID, this.ftID, this.studentID, this.month, this.day, this.reason).then(val => {
-                this.response = val
-            });
-            this.loading = false;
+            if (!this.api.isBlockedDay(this.month, this.day)) {
+                this.api.createPass(true, this.ttID, this.ftID, this.studentID, this.month, this.day, this.reason).then(val => {
+                    this.response = val;
+                    this.loading = false;
+                });
+            } else {
+                // tslint:disable-next-line:max-line-length
+                this.response = 'Error: You have selected a blocked day. If you still want to create this pass, unblock this day from  the Blocked Days tab.';
+                this.gotError = true;
+                this.loading = false;
+            }
         }
-
-        this.ngZone.run(() => {});
     }
 
     ngOnInit() {
--- src/app/requests/requests.component.ts
+++ src/app/requests/requests.component.ts
@@ -1,29 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import Card from "../card";
+import {ApiService} from "../api.service";
+import {LoginService} from "../login.service";
 
 @Component({
     selector: 'app-requests',
     templateUrl: './requests.component.html',
     styleUrls: ['./requests.component.scss']
 })
-export class RequestsComponent implements OnInit {
+export class RequestsComponent implements OnInit, AfterViewInit {
 
-    requests = ['Aidan Sacco',
-        'Guy Wilks',
-        'Daniel Longo',
-        'Alex Krantz',
-        'Peter Klopp',
-        'Uri Dickman',
-        'Connor Spackman',
-        'Cameron Snyder',
-        'Edward Newell',
-        'Dana Leland']
+    public pending: Card[];
+    public waiting: Card[];
+    public loadedP = false;
+    public loadedW = false;
 
+    constructor(private api: ApiService,
+                private loginService: LoginService) {}
 
-
-    constructor() {
-    }
 
     ngOnInit() {
+        this.loadedP = false;
+        this.loadedW = false;
+    }
+
+    async ngAfterViewInit() {
+        setTimeout(() => { this.doAPICall(); }, 10);
+    }
+
+    async doAPICall() {
+        let date = new Date();
+        let monthNum = date.getMonth();
+        let monthString = "";
+
+        if (monthNum + 1 < 10){
+            monthString = "0" + (monthNum+1).toString();
+        } else {
+            monthString = (monthNum+1).toString();
+        }
+
+        let dayNum = date.getDate();
+        let dayString = "";
+
+        if (dayNum < 10){
+            dayString = "0" + (dayNum).toString();
+        } else {
+            dayString = dayNum.toString();
+        }
+
+        this.api.getIncomingSlipsUnconditional(this.loginService.smID, monthString, dayString).then(val => {
+            try {
+                let temp = this.api.bottleCards(JSON.parse(val));
+                this.pending = temp.filter(function(card) {
+                    return card.selfIsToTeacher && card.showButtonsPending;
+                });
+            } catch(e) {
+                console.log(e);
+                this.pending = [];
+            }
+            this.loadedP = true;
+        });
+
+        this.api.getOutgoingSlipsUnconditional(this.loginService.smID, monthString, dayString).then(val => {
+            try {
+                let temp = this.api.bottleCards(JSON.parse(val));
+                this.waiting = temp.filter(function(card) {
+                    return card.selfIsToTeacher && card.showPending && !card.showButtonsPending;
+                });
+            } catch(e) {
+                console.log(e);
+                this.waiting = [];
+            }
+            this.loadedW = true;
+        });
+    }
+
+    async deny(slipID: string) {
+        if(window.confirm('Are sure you want to deny this tutorial pass? You will not be able to get it back!')) {
+            this.pending = this.pending.filter(function(card) {
+                return !(card.slipID === slipID);
+            });
+
+            this.api.denyPass(slipID);
+        }
+    }
+
+    async allow(slipID: string) {
+        this.pending = this.pending.filter(function(card) {
+            return !(card.slipID === slipID);
+        });
+
+        this.api.approvePass(slipID);
     }
 
 }
